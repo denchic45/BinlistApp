e9a0b2347066a14ee97de9636c64318c1fb0a833
denchic45 <denchic150@gmail.com>
2025-07-07 23:07:50 +0300

    Implement opening other apps on click on some data

--- app/src/main/java/com/denchic45/binlist/ui/input/BinInputUI.kt
+++ app/src/main/java/com/denchic45/binlist/ui/input/BinInputUI.kt
@@ -1,6 +1,20 @@
 package com.denchic45.binlist.ui.input
 
+import android.content.ClipData
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,9 +26,15 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Launch
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.Business
+import androidx.compose.material.icons.outlined.History
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -27,11 +47,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -43,18 +68,30 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import androidx.core.net.toUri
 import androidx.core.text.isDigitsOnly
 import com.denchic45.binlist.data.api.bin.model.BinDetailsResponse
-import com.denchic45.binlist.data.api.util.presentOrNull
+import com.denchic45.binlist.ui.theme.disabledTextColor
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BinInputScreen(viewModel: BinInputViewModel = koinViewModel()) {
+fun BinInputScreen(
+    viewModel: BinInputViewModel = koinViewModel(),
+    navigateToHistory: () -> Unit
+) {
     val input = viewModel.input
     val isValid = viewModel.isValid
     val uiState by viewModel.binDetailsUiState.collectAsState()
-    val clipboardManager = LocalClipboard.current
-
+    val context = LocalContext.current
+    val clipboard = LocalClipboard.current
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {}
+    val coroutine = rememberCoroutineScope()
     BoxWithConstraints {
         Column(
             Modifier
@@ -62,7 +99,15 @@ fun BinInputScreen(viewModel: BinInputViewModel = koinViewModel()) {
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(modifier = Modifier.height(this@BoxWithConstraints.maxHeight / 2)) {
+            CenterAlignedTopAppBar(
+                title = { Text("BinList") },
+                actions = {
+                    IconButton(onClick = navigateToHistory) {
+                        Icon(Icons.Outlined.History, null)
+                    }
+                }
+            )
+            Column(modifier = Modifier.height(this@BoxWithConstraints.maxHeight / 3)) {
                 Spacer(Modifier.weight(1f))
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     OutlinedTextField(
@@ -108,11 +153,6 @@ fun BinInputScreen(viewModel: BinInputViewModel = koinViewModel()) {
                         "Введите первые 6-8 цифр номера карты (BIN/IIN)",
                         style = MaterialTheme.typography.bodySmall
                     )
-//                repeat(20) {
-//                    ListItem(
-//                        headlineContent = { Text("Sample") },
-//                        overlineContent = { Text("overline") })
-//                }
                 }
 
                 BinDetailsUiState.Loading -> {
@@ -121,10 +161,18 @@ fun BinInputScreen(viewModel: BinInputViewModel = koinViewModel()) {
 
                 is BinDetailsUiState.Success -> BinDetailsItems(
                     state.bindDetails,
-                    onCopy = {
-
+                    onCopy = { text ->
+                        coroutine.launch {
+                            clipboard.setClipEntry(ClipEntry(ClipData.newPlainText("text", text)))
+                        }
+                        Toast.makeText(context, "Скопировано в буфер обмена", Toast.LENGTH_SHORT)
+                            .show()
                     },
-                    onExternalOpen = {}
+                    onUriOpen = { uri ->
+                        launcher.launch(
+                            Intent(Intent.ACTION_VIEW, uri.toUri())
+                        )
+                    }
                 )
 
                 BinDetailsUiState.TooManyRequests -> {
@@ -177,56 +225,155 @@ fun BinInputScreen(viewModel: BinInputViewModel = koinViewModel()) {
 fun BinDetailsItems(
     state: BinDetailsResponse,
     onCopy: (String) -> Unit,
-    onExternalOpen: () -> Unit
+    onUriOpen: (String) -> Unit
 ) {
     ListItem(
+        modifier = Modifier.combinedClickable(
+            onClick = {},
+            onLongClick = { onCopy(state.scheme) }
+        ),
         headlineContent = { Text(state.scheme) },
         overlineContent = { Text("Платежная сеть") }
     )
-
 
     ListItem(
         headlineContent = {
             Text(buildAnnotatedString {
                 withStyle(
                     style = SpanStyle(
-                        color = if (state.type == "Debit") Color.Unspecified
-                        else MaterialTheme.colorScheme.onSurface
+                        color = if (state.type == "debit") Color.Unspecified
+                        else disabledTextColor()
                     )
-                ) {
-                    append("Дебетовая")
-                }
+                ) { append("Дебетовая") }
                 append(" / ")
                 withStyle(
                     style = SpanStyle(
-                        color = if (state.type == "Credit") Color.Unspecified
-                        else MaterialTheme.colorScheme.onSurface
+                        color = if (state.type == "credit") Color.Unspecified
+                        else disabledTextColor()
                     )
-                ) {
-                    append("Кредитная")
-                }
+                ) { append("Кредитная") }
             })
         },
         overlineContent = { Text("Тип карты") }
     )
 
-    ListItem(
-        headlineContent = {
-            Text(
-                text = state.bank.name,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        },
-        overlineContent = { Text("Банк") }
+    var bankDetailsExpanded by remember { mutableStateOf(false) }
+    val elevation = animateDpAsState(
+        targetValue = if (bankDetailsExpanded) 4.dp else 0.dp,
+        label = "elevation"
     )
+    val enterTransition = remember {
+        expandVertically(
+            expandFrom = Alignment.Top,
+            animationSpec = tween()
+        )
+    }
+    val exitTransition = remember {
+        shrinkVertically(
+            shrinkTowards = Alignment.Top,
+            animationSpec = tween()
+        )
+    }
+
+
+    Column(
+        Modifier
+            .graphicsLayer { this.shadowElevation = elevation.value.toPx() }
+            .zIndex(if (bankDetailsExpanded) 1f else 0f)) {
+        ListItem(
+            modifier = Modifier
+                .let {
+                    if (state.bank.url != null)
+                        it.clickable { bankDetailsExpanded = !bankDetailsExpanded }
+                    else it
+                },
+            headlineContent = {
+                Text(
+                    text = state.bank.name,
+                    maxLines = if (bankDetailsExpanded) 2 else 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            overlineContent = { Text("Банк") },
+            trailingContent = {
+                state.bank.let { bank ->
+                    if (bank.url != null || bank.city != null || bank.phone != null) Box(Modifier) {
+                        val rotationAngle by animateFloatAsState(
+                            targetValue = if (bankDetailsExpanded) 180F else 0F,
+                            animationSpec = tween(
+                                durationMillis = 300,
+                                easing = FastOutLinearInEasing
+                            )
+
+                        )
+                        Icon(
+                            Icons.Default.KeyboardArrowDown,
+                            null,
+                            Modifier.rotate(rotationAngle)
+                        )
+                    }
+                }
+            }
+        )
+        AnimatedVisibility(
+            bankDetailsExpanded,
+            enter = enterTransition,
+            exit = exitTransition
+        ) {
+            Column {
+                state.bank.let { bank ->
+                    bank.city?.let { city ->
+                        ListItem(
+                            modifier = Modifier.combinedClickable(
+                                onClick = { onUriOpen("geo:0,0?q=$city") },
+                                onLongClick = { onCopy(city) }
+                            ),
+                            headlineContent = { Text(city) },
+                            leadingContent = {
+                                Icon(Icons.Outlined.Business, null)
+                            }
+                        )
+                    }
+                    bank.phone?.let { phone ->
+                        ListItem(
+                            modifier = Modifier.combinedClickable(
+                                onClick = { onUriOpen("tel:$phone") },
+                                onLongClick = {
+                                    onCopy(phone)
+                                }
+                            ),
+                            headlineContent = { Text(phone) },
+                            leadingContent = {
+                                Icon(Icons.Outlined.Phone, null)
+                            }
+                        )
+                    }
+                    bank.url?.let { url ->
+                        ListItem(
+                            modifier = Modifier.combinedClickable(
+                                onClick = { onUriOpen(appendProtocolIfNeeded(url)) },
+                                onLongClick = { onCopy(url) }
+                            ),
+                            headlineContent = { Text(url) },
+                            leadingContent = {
+                                Icon(Icons.AutoMirrored.Outlined.Launch, null)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
 
 
     ListItem(
+        modifier = Modifier.combinedClickable(
+            onClick = {},
+            onLongClick = { onCopy(state.brand) }
+        ),
         headlineContent = { Text(state.brand) },
         overlineContent = { Text("Платежная система") }
     )
-
     ListItem(
         headlineContent = {
             state.prepaid.onPresent { prepaid ->
@@ -234,7 +381,7 @@ fun BinDetailsItems(
                     withStyle(
                         style = SpanStyle(
                             color = if (prepaid) Color.Unspecified
-                            else MaterialTheme.colorScheme.onSurface
+                            else disabledTextColor()
                         )
                     ) {
                         append("Да")
@@ -243,28 +390,17 @@ fun BinDetailsItems(
                     withStyle(
                         style = SpanStyle(
                             color = if (!prepaid) Color.Unspecified
-                            else MaterialTheme.colorScheme.onSurface
+                            else disabledTextColor()
                         )
                     ) {
                         append("Нет")
                     }
                 })
-            }.onMissing { Text("Неизвестно") }
+            }.onMissing { Text("Неизвестно", color = disabledTextColor()) }
         },
         overlineContent = { Text("Предоплаченная") }
     )
-
-
-    var expanded by remember { mutableStateOf(false) }
-    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-        TODO()
-    }
-
     ListItem(
-        modifier = Modifier.combinedClickable(
-            onClick = {},
-            onLongClick = {}
-        ),
         headlineContent = {
             Row {
                 Row(Modifier.weight(1f)) {
@@ -272,39 +408,26 @@ fun BinDetailsItems(
                     state.number.length.onPresent { length ->
                         Text(length.toString())
                     }.onMissing {
-                        Text("неизвестно", color = MaterialTheme.colorScheme.onSurface)
+                        Text("неизвестно", color = disabledTextColor())
                     }
                 }
                 Row(Modifier.weight(1f)) {
                     Text("Lugn - ")
-                    Text(buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = if (state.number.luhn.presentOrNull() == true)
-                                    Color.Unspecified
-                                else MaterialTheme.colorScheme.onSurface
-                            )
-                        ) {
-                            append("Есть")
-                        }
-                        append(" / ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = if (state.number.luhn.presentOrNull() == false)
-                                    Color.Unspecified
-                                else MaterialTheme.colorScheme.onSurface
-                            )
-                        ) {
-                            append("Нет")
-                        }
-                    })
+                    state.number.luhn.onPresent { luhn ->
+                        Text(if (luhn) "есть" else "нет")
+                    }.onMissing {
+                        Text("неизвестно", color = disabledTextColor())
+                    }
                 }
             }
         },
         overlineContent = { Text("Номер карты") }
     )
-
     ListItem(
+        modifier = Modifier.combinedClickable(
+            onClick = { onUriOpen("geo:0,0?q=${state.country.name}") },
+            onLongClick = { onCopy(state.country.name) }
+        ),
         headlineContent = { Text(state.country.emoji + " " + state.country.name) },
         overlineContent = { Text("Страна") },
         supportingContent = {
@@ -315,7 +438,6 @@ fun BinDetailsItems(
             )
         }
     )
-
 }
 
 class BinTransformation : VisualTransformation {
@@ -337,4 +459,9 @@ class BinTransformation : VisualTransformation {
             }
         )
     }
+}
+
+private fun appendProtocolIfNeeded(url: String): String {
+    return if (!url.startsWith("http://") || !url.startsWith("https://"))
+        "http://$url" else url
 }
