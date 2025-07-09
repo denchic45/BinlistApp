3495613e1bda21ce67bc5d6043a8e0d0b3e18350
denchic45 <denchic150@gmail.com>
2025-07-09 18:54:01 +0300

    Implement to get cached requests by id instead of by bin

--- app/src/main/java/com/denchic45/binlist/di/module/DomainModule.kt
+++ app/src/main/java/com/denchic45/binlist/di/module/DomainModule.kt
@@ -1,9 +1,11 @@
 package com.denchic45.binlist.di.module
 
 import com.denchic45.binlist.domain.usecase.FindBinDetailsUseCase
+import com.denchic45.binlist.domain.usecase.FindSaveBinDetailsUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
     includes(dataModule)
     single { FindBinDetailsUseCase(get()) }
+    single { FindSaveBinDetailsUseCase(get()) }
 }
--- app/src/main/java/com/denchic45/binlist/domain/usecase/FindSaveBinDetailsUseCase.kt
+++ app/src/main/java/com/denchic45/binlist/domain/usecase/FindSaveBinDetailsUseCase.kt
@@ -1,11 +1,11 @@
 package com.denchic45.binlist.domain.usecase
 
-import com.denchic45.binlist.data.api.bin.model.BinDetailsResponse
 import com.denchic45.binlist.data.repository.BinRepository
+import com.denchic45.binlist.domain.model.BinDetailsRequest
 import kotlinx.coroutines.flow.Flow
 
 class FindSaveBinDetailsUseCase(private val binRepository: BinRepository) {
-    operator fun invoke(): Flow<List<BinDetailsResponse>> {
+    operator fun invoke(): Flow<List<BinDetailsRequest>> {
         return binRepository.findSavedBinDetails()
     }
 }
