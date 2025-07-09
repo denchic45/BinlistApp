fc3fdb7d425d0431b9598c7aa789d293bd9427aa
denchic45 <denchic150@gmail.com>
2025-07-09 18:43:20 +0300

    Fix check nullable response result

--- app/src/main/java/com/denchic45/binlist/data/api/bin/BinListApi.kt
+++ app/src/main/java/com/denchic45/binlist/data/api/bin/BinListApi.kt
@@ -7,6 +7,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonObject
 
@@ -17,7 +18,7 @@ class BinListApi(private val client: HttpClient) {
             request = { get("https://lookup.binlist.net/$bin") },
             map = {
                 json.parseToJsonElement(bodyAsText())
-                    .takeIf { element -> element.jsonObject["number"] != null }
+                    .takeIf { element -> element.jsonObject["number"] !is JsonNull }
                     ?.let { json.decodeFromJsonElement(it) }
             })
     }
